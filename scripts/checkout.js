87134fcf81113d342973c77040a4cdc6ab3d940a
ANANTIA TEMGOUA <elvado.temgoua@gmail.com>
2025-07-01 07:07:20 +0100

    All working

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import { car1 } from "../data/car.js";
 
 renderOrderSummary();
 renderPaymentSummary();
